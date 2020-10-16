fb353132e2f2dce98d54146f11c4eaed9966034d
akitasummer <644171127@qq.com>
2020-10-17 01:25:18 +0800

    Tabs init

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,16 +1,28 @@
 <template>
 <div class="cheese-tabs">
-  <div class="cheese-tabs-nav">
-    <div class="cheese-tabs-nav-item" @click="select(t)" :class="{selected: t === selected}" v-for="(t, i) in titles" :key="Date.now() + i">{{t}}</div>
+  <div class="cheese-tabs-nav" ref="container">
+    <div
+        class="cheese-tabs-nav-item"
+        @click="select(t)"
+        :class="{selected: t === selected}"
+        v-for="(t, i) in titles"
+        :key="Date.now() + i"
+        :ref="el => { if (t === selected) { selectedItem = el } }"
+    >{{t}}</div>
+    <div class="cheese-tabs-nav-indicator" ref="indicator"></div>
   </div>
   <div class="cheese-tabs-content">
-    <component class="cheese-tabs-content-item" :is="current"></component>
+    <component
+        class="cheese-tabs-content-item"
+        :is="current"
+        :key="current.props.title"
+    ></component>
   </div>
 </div>
 </template>
 
 <script lang="ts">
-import {defineComponent, onMounted, ref, computed, toRefs, Slot} from 'vue'
+import {defineComponent, ref, computed, watchEffect, onMounted} from 'vue'
 import Tab from "./Tab.vue";
 export default defineComponent({
   name: "Tabs",
@@ -23,34 +35,62 @@ export default defineComponent({
   setup(props, { slots, emit }) {
     const defaults = slots.default?.()
     const titles = ref<string[]>([])
-    const { selected } = toRefs(props)
+    const selectedItem = ref<HTMLElement>()
+    const indicator = ref<HTMLElement>()
+    const container = ref<HTMLElement>()
     const select = (title: string) => {
       emit('update:selected', title)
     }
-    onMounted(() => {
-      if (defaults) {
-        defaults.forEach((tag) => {
-          if(JSON.stringify(tag.type) !== JSON.stringify(Tab)) {
-            throw new Error('Tabs 字标签必须是 Tab')
-          }
-        })
-        titles.value = defaults.map((tag) => {
-          return tag.props?.title
-        })
-      }
-    })
     const current = computed(() => {
-      const result = defaults?.filter((tag) => {
+      return defaults?.find((tag) => {
         return tag.props?.title === props.selected
-      })[0]?.children
-      if (result && typeof result !== 'string' && !Array.isArray(result)) {
-      }
+      })
     })
+
+    if (defaults) {
+      defaults.forEach((tag) => {
+        if(JSON.stringify(tag.type) !== JSON.stringify(Tab)) {
+          throw new Error('Tabs 字标签必须是 Tab')
+        }
+      })
+      titles.value = defaults.map((tag) => {
+        return tag.props?.title
+      })
+    }
+
+    const setIndicator = () => {
+      const result = selectedItem.value
+      const { width } = result ? result.getBoundingClientRect() : { width: 0 }
+      const { left: containerLeft } = container.value ? container.value.getBoundingClientRect() : { left: 0 }
+      const { left: resultLeft } = result ? result.getBoundingClientRect() : { left: 0 }
+      const left = resultLeft - containerLeft
+      if (indicator.value) {
+        indicator.value.style.left = left + 'px'
+        indicator.value.style.width = width + 'px'
+      }
+    }
+
+    onMounted(() => {
+      watchEffect(() => {
+        setIndicator()
+      },{
+        flush: 'post'
+      })
+      setTimeout(() => {
+        if (indicator.value) {
+          indicator.value.style.transition = 'all 250ms'
+        }
+      }, 0)
+    })
+
     return {
       defaults,
       titles,
       select,
-      current
+      current,
+      selectedItem,
+      indicator,
+      container
     }
   }
 })
@@ -83,8 +123,8 @@ $border-color: #d9d9d9;
       background: $blue;
       left: 0;
       bottom: -1px;
-      width: 100px;
-      transition: all 250ms;
+      width: 0;
+      //transition: all 250ms;
     }
   }
   &-content {
